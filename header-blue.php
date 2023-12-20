3c13321e77dd79758534fbe03988511570f8af12
BENDJEDDOU-Rayan-2225011a <rayan.bendjeddou@etu.univ-amu.fr>
2023-12-20 16:30:02 +0100

    fix header

--- header-blue.php
+++ header-blue.php
@@ -11,6 +11,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/assets/css/blue_header.css' ?>">
     <?php wp_head(); ?>
 </head>
 <!-- BODY -->
@@ -24,12 +25,11 @@ if(in_array('television', $current_user->roles)) : ?>
 <!-- HEADER -->
 <?php $current_user = wp_get_current_user();
 if(!in_array('television', $current_user->roles)) : ?>
-    <header class="text-white" style="background-color: #1476c6;">
+    <header>
         <div class="header-bg">
-            <div class="header">
-                <img src="https://iut.univ-amu.fr/themes/amu/amu_iut/logo.svg" alt="logo-iut">
-                <h1 id="titre-header">ACCUEIL</h1>
-                <h1> </h1>
+            <div class="header-content">
+                <img src="https://iut.univ-amu.fr/themes/amu/amu_iut/logo.svg" alt="logo-iut" id="img-iut">
+                <h1 id="titre"><?php echo single_post_title(); ?></h1>
             </div>
         </div>
     </header>
