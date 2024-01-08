e57ed57f6a8b09c683bf92df9b9c934ddd94ff28
Matteo <matteo.sciacca@etu.univ-amu.fr>
2024-01-08 15:20:53 +0100

    Footer sans lien

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 <?php ?>
 <!-- FOOTER -->
-<footer class="mt-auto py-3 text-white" style="background-color: #1476c6;">
-  <div class="container">
-    <p class="text-center">© 2021 - Aix-Marseille Université</p>
-  </div>
+<footer class="mt-auto py-3 text-white" >
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/assets/css/footer.css' ?>">
+    <div class="container">
+        <img id="fleche" src='/wp-content/themes/theme-ecran-connecte/assets/images/en-arriere.png'>
+        <p class="text-center" id="texte"> RETOUR </p>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
