14bfb8aebb6332f1a1778e5eead071374fbf6cdd
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2024-01-08 16:36:03 +0100

    Retour en arrière fonctionnel

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer class="mt-auto py-3 text-white" >
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/assets/css/footer.css' ?>">
     <div class="container">
-        <img id="fleche" src='/wp-content/themes/theme-ecran-connecte/assets/images/en-arriere.png'>
+        <img id="fleche" src='/wp-content/themes/theme-ecran-connecte/assets/images/en-arriere.png' onclick="history.back()">
         <p class="text-center" id="texte"> RETOUR </p>
     </div>
 </footer>
