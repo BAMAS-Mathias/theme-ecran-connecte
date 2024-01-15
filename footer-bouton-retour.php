27e972d3972e42907e21ffed3364591432a5c13e
ByOnyx <byonyxmc@gmail.com>
2024-01-15 14:59:40 +0100

    Ajout d'une animation sur le bouton retour

--- footer-bouton-retour.php
+++ footer-bouton-retour.php
@@ -17,14 +17,23 @@
     .container-retour {
         display: flex;
         justify-content: flex-end;
-        padding-right: 15%;
+        padding-right: 5vw;
         padding-bottom: 5%;
     }
+
+    .container-retour a{
+        cursor: pointer;
+        transition: all 0.15s ease;
+    }
+    .container-retour a:hover{
+        transform: scale(1.1);
+    }
+
 </style>
 <footer >
     <div class="container-retour">
         <a onclick="history.back()">
-            <img id="fleche-icon" src='/wp-content/themes/theme-ecran-connecte/assets/images/en-arriere.png'>
+            <img id="fleche-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/en-arriere.png" alt="Logo retour">
             <h6 id="texte"> RETOUR </h6>
         </a>
     </div>
