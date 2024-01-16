b4079e912dfa043d249bf9ab93bd1a71db012b67
d22003660 <raphael.DE-SOUSA-GUEDES@etu.univ-amu.fr>
2024-01-16 15:47:21 +0100

    Mise en place du bouton iption et changement de date

--- footer-bouton-retour.php
+++ footer-bouton-retour.php
@@ -16,18 +16,22 @@
 
     .container-retour {
         display: flex;
-        justify-content: flex-end;
-        padding-right: 10%;
+        justify-content: space-between;
         padding-bottom: 5%;
+        width: 80vw;
+        margin: 0 auto;
     }
+
 </style>
-<footer >
-    <div class="container-retour">
-        <a onclick="history.back()">
-            <img id="fleche-icon" src='/wp-content/themes/theme-ecran-connecte/assets/images/en-arriere.png'>
-            <h6 id="texte"> RETOUR </h6>
-        </a>
-    </div>
+<div class="container-retour">
+    <a href="https://ecran1.alwaysdata.net/secretary/config/">
+        <img id="fleche-icon" src='/wp-content/themes/theme-ecran-connecte/assets/images/Option.png'>
+        <h6 id="texte"> OPTION </h6>
+    </a>
+    <a onclick="history.back()">
+        <img id="fleche-icon" src='/wp-content/themes/theme-ecran-connecte/assets/images/en-arriere.png'>
+        <h6 id="texte"> RETOUR </h6>
+    </a>
+</div>
 </footer>
 <?php wp_footer(); ?>
-</body>
--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <!-- FOOTER -->
 <footer class="mt-auto py-3 text-white" style="background-color: #1476c6;">
   <div class="container">
-    <p class="text-center">© 2021 - Aix-Marseille Université</p>
+    <p class="text-center">© 2024 - Aix-Marseille Université</p>
   </div>
 </footer>
 <?php wp_footer(); ?>
